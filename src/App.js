651ac188f1a79928324b180a6a59a4da8049a4ec
Suyash <suyu162001@gmail.com>
2021-08-07 00:37:46 +0530

    ✅ReDesigned Ui- elements ✅ Removed useless code from the styles ✅ Improved Animations 🥵Xetra version two Redesigned

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ function App() {
   return (
     <div className="App">
 		  <Navigation/>
-      <div className="Hero boxwidth">
-        <div className={`Hero_subtext`}>
-          <h2>Enter the ultimate shopping experience.</h2>
-          <div className="Option-box">
-            <button>
-              <div className="button-content">
-                <h3>Sign up with Google</h3>
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M39.8122 20.5513C39.8122 31.8239 32.0504 39.8462 20.588 39.8462C9.59818 39.8462 0.723007 31.0193 0.723007 20.0892C0.723007 9.15909 9.59818 0.332153 20.588 0.332153C25.9387 0.332153 30.4404 2.28396 33.9088 5.50244L28.502 10.6727C21.4291 3.88523 8.27652 8.98383 8.27652 20.0892C8.27652 26.9803 13.8115 32.5648 20.588 32.5648C28.4539 32.5648 31.4016 26.9564 31.8662 24.0486H20.588V17.2531H39.4998C39.6841 18.2649 39.8122 19.2368 39.8122 20.5513Z" fill="#1F2123"/>
-                </svg>
-              </div>
-            </button>
-            <h3 className="button_sub">Or use email to <span>Sign Up</span></h3>
+        <div className="Hero boxwidth">
+          <div className={"Hero_subtext"}>
+            <h2>Your <nobr>one-stop</nobr> for casuals is one click away</h2>
+            <div className="Option-box">
+              <button>
+                <div className="button-content">
+                  <h3>Sign up with Google</h3>
+                  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M39.8122 20.5513C39.8122 31.8239 32.0504 39.8462 20.588 39.8462C9.59818 39.8462 0.723007 31.0193 0.723007 20.0892C0.723007 9.15909 9.59818 0.332153 20.588 0.332153C25.9387 0.332153 30.4404 2.28396 33.9088 5.50244L28.502 10.6727C21.4291 3.88523 8.27652 8.98383 8.27652 20.0892C8.27652 26.9803 13.8115 32.5648 20.588 32.5648C28.4539 32.5648 31.4016 26.9564 31.8662 24.0486H20.588V17.2531H39.4998C39.6841 18.2649 39.8122 19.2368 39.8122 20.5513Z" fill="#1F2123"/>
+                  </svg>
+                </div>
+              </button>
+              <h3 className="button_sub">Or use email to <span>Sign Up</span></h3>
+            </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Sidebar({ Open, children}) {
+function Sidebar({ Open, children, setOpen}) {
   return (
     <div className="wrapper">
-      <div className={`blur ${Open && "blur_show"}`}></div>
-      <div className={`sidebar ${Open && "sidebar_show"}`}>
+      {Open && <div className="ClickToExit" onClick={()=>setOpen(!Open)}/>}
+      <div className={`blur ${Open? "blur_show":"blur_hide"}`}></div>
+      <div className={`sidebar ${Open? "sidebar_show":"sidebar_hide"}`}>
         {children}
       </div>
     </div>
